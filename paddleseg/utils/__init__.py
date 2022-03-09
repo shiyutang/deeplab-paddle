fdb38d9d13207575d4cbc2ebdbe2c54ab02fcb3f
shiyutang <1574572981@qq.com>
2022-03-09 17:26:15 +0800

    export

--- paddleseg/utils/__init__.py
+++ paddleseg/utils/__init__.py
@@ -20,4 +20,41 @@ from .utils import *
 from .timer import TimeAverager, calculate_eta
 from . import visualize
 from .config_check import config_check
-from .crf import DenseCRF
+# from .crf import DenseCRF
+
+def get_image_list(image_path):
+    """Get image list"""
+    valid_suffix = [
+        '.JPEG', '.jpeg', '.JPG', '.jpg', '.BMP', '.bmp', '.PNG', '.png'
+    ]
+    image_list = []
+    image_dir = None
+    if os.path.isfile(image_path):
+        if os.path.splitext(image_path)[-1] in valid_suffix:
+            image_list.append(image_path)
+        else:
+            image_dir = os.path.dirname(image_path)
+            with open(image_path, 'r') as f:
+                for line in f:
+                    line = line.strip()
+                    if len(line.split()) > 1:
+                        line = line.split()[0]
+                    image_list.append(os.path.join(image_dir, line))
+    elif os.path.isdir(image_path):
+        image_dir = image_path
+        for root, dirs, files in os.walk(image_path):
+            for f in files:
+                if '.ipynb_checkpoints' in root:
+                    continue
+                if os.path.splitext(f)[-1] in valid_suffix:
+                    image_list.append(os.path.join(root, f))
+    else:
+        raise FileNotFoundError(
+            '`--image_path` is not found. it should be a path of image, or a file list containing image paths, or a directory including images.'
+        )
+
+    if len(image_list) == 0:
+        raise RuntimeError(
+            'There are not image file in `--image_path`={}'.format(image_path))
+
+    return image_list, image_dir
